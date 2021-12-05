f7cd8ab765a520c5d5a2ad3daaa564c67890a736
Adi Bar Ilan <adib@securitydam.com>
2021-12-05 07:53:21 +0200

    remove day num from template

--- template/solution.py
+++ template/solution.py
@@ -1,9 +1,9 @@
 def q1():
-    with open('day 5\input.txt', 'r') as f:
+    with open('day \input.txt', 'r') as f:
         input = f.readlines()
     
 def q2():
-    with open('day 5\input.txt', 'r') as f:
+    with open('day \input.txt', 'r') as f:
         input = f.readlines()
 
 if __name__ == '__main__':
