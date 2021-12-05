84eaad8ce033618bb8b6c81b48e2e699dc691d18
Adi Bar Ilan <adib@securitydam.com>
2021-12-05 06:41:40 +0200

    Begin day 5 and add a template folder

---
+++ template/solution.py
@@ -0,0 +1,11 @@
+def q1():
+    with open('day 5\input.txt', 'r') as f:
+        input = f.readlines()
+    
+def q2():
+    with open('day 5\input.txt', 'r') as f:
+        input = f.readlines()
+
+if __name__ == '__main__':
+    print(f'Part 1: {q1()}')
+    print(f'Part 2: {q2()}')
