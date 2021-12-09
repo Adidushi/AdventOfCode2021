3d50db4619ad8c48199bbd7da12f56009c6a0d63
Adi Bar Ilan <adib@securitydam.com>
2021-12-09 10:44:27 +0200

    Add splitlines to avoid cringe newline characters

--- template/solution.py
+++ template/solution.py
@@ -1,10 +1,10 @@
 def q1():
     with open('day \input.txt', 'r') as f:
-        input = f.readlines()
+        input = f.read().splitlines()
     
 def q2():
     with open('day \input.txt', 'r') as f:
-        input = f.readlines()
+        input = f.read().splitlines()
 
 if __name__ == '__main__':
     print(f'Part 1: {q1()}')
