f62bdf9f5cbe2c5dd2a55490cd895fed9755bc6a
Adi Bar Ilan <adibarilan@gmail.com>
2021-12-11 14:49:31 +0200

    Fix the code

--- day 11/solution.py
+++ day 11/solution.py
@@ -1,74 +1,87 @@
-import sys
-sys.setrecursionlimit(1000)
+def flashes_left(matrix, flashed):
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if matrix[r][c] > 9 and (r, c) not in flashed:
+                return True
+    return False
+
 
 def get_neighbors(matrix, flashed, r, c):
     neighbors = ((r-1, c-1), (r-1, c), (r-1, c+1),
                  (r, c-1), (r, c+1),
                  (r+1, c-1), (r+1, c), (r+1, c+1))
     for r, c in neighbors:
-        if 0 <= r < len(matrix) and 0 <= c < len(matrix[0]) and matrix[r][c] not in flashed:
+        if 0 <= r < len(matrix) and 0 <= c < len(matrix[0]) and (r, c) not in flashed:
             yield (r, c)
 
 
-def keep_flashing(matrix):
-    for r_num, r in enumerate(matrix):
-        for c_num, c in enumerate(r):
-            if c > 9 and (r_num, c_num):
-                return True
-    return False
-
-
 def flash(matrix, flashed, r, c):
-    matrix[r][c] = 0
-    flashed.add((r, c))
-    for n_r, n_c in get_neighbors(matrix, flashed, r, c):
-        if matrix[n_r][n_c] > 9:
-            flash(matrix, flashed, r, c)
+    neighbors = get_neighbors(matrix, flashed, r, c)
+    for n_r, n_c in neighbors:
+        matrix[n_r][n_c] += 1
+
+    flashed.append((r, c))
+
+
+def flash_all(matrix, flashed):
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if matrix[r][c] > 9 and ((r, c) not in flashed):
+                flash(matrix, flashed, r, c)
+                return 1
 
 
 def do_step(matrix):
-    print('new step!')
-    flashed = set()
+    flashed = list()
+    flashed_count = 0
 
-    # Add 1 to all octis
     for r in range(len(matrix)):
-        for c in range(r):
+        for c in range(len(matrix[0])):
             matrix[r][c] += 1
 
-    flash_total = 0
+    while flashes_left(matrix, flashed):
+        flash_all(matrix, flashed)
+        flashed_count += 1
 
-    to_add = set()
+    for r, c in flashed:
+        matrix[r][c] = 0
 
-    while keep_flashing(matrix):
-        for r, c in to_add:
-            matrix[r][c] += 1
-        to_add = set()
-        for r in range(len(matrix)):
-            for c in range(r):
-                if matrix[r][c] > 9 and matrix[r][c] not in flashed:
-                    flash(matrix, flashed, r, c)
-                    # matrix[r][c] = 0
-                    # flash_total += 1
-                    # for n_r, n_c in get_neighbors(matrix, flashed, r, c):
-                    #     matrix[n_r][n_c] += 1
-
-    return flash_total
+    return flashed_count
 
 
 def q1(steps):
     with open('day 11\input.txt', 'r') as f:
         input = f.read().splitlines()
     matrix = list(map(list, input))
+
     for r_num, r in enumerate(matrix):
         for c_num, c in enumerate(r):
             matrix[r_num][c_num] = int(c)
-    total = sum([do_step(matrix) for _ in range(steps)])
-    return total
+
+    total_flashes = 0
+
+    for _ in range(steps):
+        total_flashes += do_step((matrix))
+
+    return total_flashes
 
 
 def q2():
     with open('day 11\input.txt', 'r') as f:
         input = f.read().splitlines()
+    matrix = list(map(list, input))
+
+    for r_num, r in enumerate(matrix):
+        for c_num, c in enumerate(r):
+            matrix[r_num][c_num] = int(c)
+
+    counter = 1
+
+    while True:
+        step_flashes = do_step((matrix))
+        if step_flashes == 100:
+            return counter
+        counter += 1
 
 
 if __name__ == '__main__':
