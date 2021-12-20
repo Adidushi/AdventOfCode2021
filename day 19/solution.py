a72fc82ac31754a8a476d8b64c009f9e89b23ac5
Adi Bar Ilan <adibarilan@gmail.com>
2021-12-20 10:39:19 +0200

    Add to day 19

--- day 19/solution.py
+++ day 19/solution.py
@@ -7,38 +7,46 @@ ALL_PERMS = [(axis, direction) for axis, direction in product(AXES, DIRS)]
 
 
 class Scanner:
-    def __init__(self, points: str):
+    def __init__(self, points: str, index):
         lines = points.splitlines()
-        self.points = np.array([[int(x) for x in l.split(',')] for l in lines[1:]], dtype='int16')
+        self.points = np.array([[int(x) for x in l.split(',')]
+                               for l in lines[1:]], dtype='int16')
         self.pos = None
+        self.index = index
 
     def find_offset(self, other: 'Scanner'):
-        if other.pos is not None: return False
+        if other.pos is not None:
+            return False
         my_points_cp = self.points
         for ax, d in ALL_PERMS:
             other_points_cp = other.points[:, ax - 1] * d
             d = my_points_cp[np.newaxis, :] - other_points_cp[:, np.newaxis]
-            uniques = [np.unique(d[..., i], return_counts=True) for i in range(3)]
+            uniques = [np.unique(d[..., i], return_counts=True)
+                       for i in range(3)]
 
             if all([max(unique[1]) >= 12 for unique in uniques]):
+
                 other.points = other_points_cp
-                other.pos = np.array([unique[0][unique[1] >= 12][0] for unique in uniques], dtype='int16') + self.pos
+                other.pos = np.array([unique[0][unique[1] >= 12][0]
+                                     for unique in uniques], dtype='int16') + self.pos
+
+                # print(f'found {other.index}: {other.pos}')
                 return True
 
         return False
 
 
 def q1():
-    with open('day 19\input.txt', 'r') as f:
+    with open('day 19\sample.txt', 'r') as f:
         input = f.read().split('\n\n')
 
-    scanners = [Scanner(block) for block in input]
+    scanners = [Scanner(block, index) for index, block in enumerate(input)]
     scanners[0].pos = np.array([0, 0, 0])
 
     while any(s.pos is None for s in scanners):
-        for s in filter(lambda x: x.pos is None, scanners):
-            for posed in scanners:
-                if posed.pos is not None and posed.find_offset(s):
+        for unknown in filter(lambda x: x.pos is None, scanners):
+            for known in scanners:
+                if known.pos is not None and known.find_offset(unknown):
                     break
 
     beacons = set()
@@ -48,7 +56,7 @@ def q1():
     return len(beacons), scanners
 
 
-def q2(scanners):    
+def q2(scanners):
     farthest = 0
     for scanner_1 in scanners:
         for scanner_2 in scanners:
@@ -57,6 +65,7 @@ def q2(scanners):
                 farthest = max(farthest, distance)
     return farthest
 
+
 def visualize(scanners):
     import plotly.express as px
     import pandas as pd
@@ -64,18 +73,18 @@ def visualize(scanners):
     data = list()
     for scanner in scanners:
         for point in (scanner.points + scanner.pos):
-            data.append((*point, 'Beacon'))
-        data.append((*scanner.pos, 'Scanner'))
-    data.append((0, 0, 0, 'Origin'))
+            data.append((*point, 'Beacon', 0))
+        data.append((*scanner.pos, 'Scanner', scanner.index))
+    data.append((0, 0, 0, 'Origin', 0))
 
-    data = pd.DataFrame(data, columns=['x', 'y', 'z', 'type'])
+    data = pd.DataFrame(data, columns=['x', 'y', 'z', 'type', 'index'])
     fig = px.scatter_3d(data, x='x', y='y', z='z', color='type', symbol='type')
     fig.show()
 
 
 if __name__ == '__main__':
     from time import perf_counter as pc
-    
+
     st = pc()
     ans, scanners = q1()
     print(f'Part 1: {ans}')
